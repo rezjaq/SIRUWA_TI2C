d284813926dcb498e1fc14f2ee739727bc9a4b0d
rhanilham <rhanilhamfadlillatulr@gmail.com>
2024-05-22 11:11:56 +0700

    CRUD RT: Warga, Keluarga, Pengumuman, Kegiatan, Pengaduan

--- resources/views/template-admin/sidebar.blade.php
+++ resources/views/template-admin/sidebar.blade.php
@@ -1,32 +1,38 @@
 <nav class="sidebar sidebar-offcanvas" id="sidebar">
     <ul class="nav">
         <li class="nav-item">
-            <a href="{{ url('/') }}" class="nav-link {{ ($activeMenu == 'dashboard') ? 'active' : '' }}">
+            <a href="{{ url('/RT') }}" class="nav-link {{ ($activeMenu == 'dashboard') ? 'active' : '' }}">
                 <span class="menu-title">Dashboard</span>
                 <i class="mdi mdi-home menu-icon"></i>
             </a>
         </li>
         <li class="nav-item">
-            <a href="{{ url('/keluarga') }}" class="nav-link {{ ($activeMenu == 'keluarga') ? 'active' : '' }}">
-                <span class="menu-title">keluarga</span>
-                <i class="mdi mdi-credit-card-multiple menu-icon"></i>
+            <a href="{{ url('/family') }}" class="nav-link {{ ($activeMenu == 'family') ? 'active' : '' }}">
+                <span class="menu-title">Keluarga</span>
+                <i class="mdi mdi-account-multiple menu-icon"></i>
             </a>
         </li>
         <li class="nav-item">
-            <a href="{{ url('/warga') }}" class="nav-link {{ ($activeMenu == 'Warga') ? 'active' : '' }}">
-                <span class="menu-title">warga</span>
-                <i class="mdi mdi-credit-card-multiple menu-icon"></i>
+            <a href="{{ url('/citizen') }}" class="nav-link {{ ($activeMenu == 'citizen') ? 'active' : '' }}">
+                <span class="menu-title">Warga</span>
+                <i class="mdi mdi-account menu-icon"></i>
             </a>
         </li>
         <li class="nav-item">
-            <a href="{{ url('/pengumuman') }}" class="nav-link {{ ($activeMenu == 'pengumuman') ? 'active' : '' }}">
+            <a href="{{ url('/announcement') }}" class="nav-link {{ ($activeMenu == 'announcement') ? 'active' : '' }}">
                 <span class="menu-title">Pengumuman</span>
-                <i class="mdi mdi-credit-card-multiple menu-icon"></i>
+                <i class="mdi mdi-clipboard-text menu-icon"></i>
             </a>
         </li>
         <li class="nav-item">
-            <a href="{{ url('/kegiatan') }}" class="nav-link {{ ($activeMenu == 'kegiatan') ? 'active' : '' }}">
+            <a href="{{ url('/activity') }}" class="nav-link {{ ($activeMenu == 'activity') ? 'active' : '' }}">
                 <span class="menu-title">Kegiatan</span>
+                <i class="mdi mdi-calendar menu-icon"></i>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="{{ url('/complaint') }}" class="nav-link {{ ($activeMenu == 'complaint') ? 'active' : '' }}">
+                <span class="menu-title">Pengaduan</span>
                 <i class="mdi mdi-comment-alert menu-icon"></i>
             </a>
         </li>
