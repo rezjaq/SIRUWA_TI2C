e86e62471a741a2bdcdc8c781ef04b86fd4c0664
Rizqi Reza Danuarta <ejaaa646@gmail.com>
2024-05-29 19:34:27 +0700

    fixing sidebar

--- resources/views/template-admin/sidebar.blade.php
+++ resources/views/template-admin/sidebar.blade.php
@@ -47,8 +47,7 @@
             </a>
         </li>
         <li class="nav-item">
-            <a href="{{ url('/complaint') }}" class="nav-link {{ ($activeMenu == 'complaint') ? 'active' : '' }}">
-            <a href="{{ url('/Pengaduann') }}" class="nav-link {{ ($activeMenu == 'complaint') ? 'active' : '' }}">
+            <a href="{{ url('/Pengaduann') }}" class="nav-link {{ ($activeMenu == 'Pengaduan') ? 'active' : '' }}">
                 <span class="menu-title">Pengaduan</span>
                 <i class="mdi mdi-comment-alert menu-icon"></i>
             </a>
